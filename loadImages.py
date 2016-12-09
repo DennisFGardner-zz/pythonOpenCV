4827aa8b33da0cdc49490e72a2eb34e9a12d669e
Dennis Gardner <dennis.gardner@colorado.edu>
2016-12-09 10:29:27 -0800

    got PIL working

--- loadImages.py
+++ loadImages.py
@@ -1,10 +1,28 @@
 # is this how I make comments
 
-import numpy as np
-import cv2
+#import numpy as np
+#import cv2
+#
+#img = cv2.imread('headshot.jpg',0)
+#
+#cv2.imshow('image',img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 
-img = cv2.imread('headshot.jpg',0)
+from pylab import *
+#import Image
 
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+from PIL import Image
+
+im = Image.open('headshot.jpg').convert('L')
+#im = im.convert('L')
+
+#print im
+
+print 2
+
+im = array(im)
+
+imshow(im)
+
+print 'helloworld'
